159d7689496b7e399698a030e0adb161d9e1ce04
Venugopal <venugpl2000@gmail.com>
2022-05-22 13:04:07 +0530

    Worked on Login(Main Activity) and Failed brutally...lol

--- app/src/main/java/com/example/projectrefill/MainActivity.java
+++ app/src/main/java/com/example/projectrefill/MainActivity.java
@@ -19,15 +19,76 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
     Button btn_login;
-    TextInputLayout username, password;
+    EditText username,u_password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        btn_login = findViewById(R.id.btn_login);
+        username = findViewById(R.id.txtUser);
+        u_password = findViewById(R.id.txtPassword);
 
     }
+        public void onClick (View view){
+            String fusername = username.getText().toString().trim();
+            String fpassword = u_password.getText().toString().trim();
+
+            Toast.makeText(this, "Onclick running", Toast.LENGTH_SHORT).show();
+
+            Query chkUser = FirebaseDatabase.getInstance().getReference("Client").orderByChild("username").equalTo(fusername);
+            chkUser.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.exists()) {
+                        username.setError(null);
+                        Toast.makeText(MainActivity.this, "Client exist", Toast.LENGTH_SHORT).show();
+
+                        String t_password = dataSnapshot.child(fpassword).getValue(String.class);
+                        if (t_password.equals(fpassword)) {
+                            u_password.setError(null);
+                            Toast.makeText(MainActivity.this, "Client password wrong", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    Toast.makeText(MainActivity.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+
+                }
+            });
+            Query chkRetailer = FirebaseDatabase.getInstance().getReference("Retailer").orderByChild("username").equalTo(fusername);
+            chkRetailer.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.exists()) {
+                        username.setError(null);
+                        Toast.makeText(MainActivity.this, "User exists", Toast.LENGTH_SHORT).show();
+
+                        String t_password = dataSnapshot.child(fusername).child("password").getValue(String.class);
+                        if (t_password.equals(fpassword)) {
+                            u_password.setError(null);
+                            Toast.makeText(MainActivity.this, "Password is right", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Wrong Password", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(MainActivity.this, "User do not exists", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    Toast.makeText(MainActivity.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+
+                }
+            });
+        }
+
 
 
 }
