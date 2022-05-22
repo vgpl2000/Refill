408168c2e14cd042e12abe7041b49dbd99820cf6
Venugopal <venugpl2000@gmail.com>
2022-05-22 12:09:00 +0530

    Tried and failed to login

--- app/src/main/java/com/example/projectrefill/MainActivity.java
+++ app/src/main/java/com/example/projectrefill/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.projectrefill;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_login;
+    TextInputLayout username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
 }
