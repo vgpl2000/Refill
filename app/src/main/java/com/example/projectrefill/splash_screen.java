3b842230336af6cbb6fe4c928d2e8c3a2b4f6495
Akash <akashcbhat2001@gmail.com>
2022-05-29 15:53:00 +0530

    splash corrected

--- app/src/main/java/com/example/projectrefill/splash_screen.java
+++ app/src/main/java/com/example/projectrefill/splash_screen.java
@@ -3,40 +3,36 @@ package com.example.projectrefill;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.MediaController;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.VideoView;
 
 public class splash_screen extends AppCompatActivity {
-
-    VideoView videoView;
+    TextView title_text,textView2;
+ImageView imageView,imageView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-
-        videoView=findViewById(R.id.videoView);
-
-        String videopath="android.resource://com.example.projectrefill/"+R.raw.splash;
-        Uri uri=Uri.parse(videopath);
-        videoView.setVideoURI(uri);
-        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mediaPlayer) {
-                mediaPlayer.start();
-            }
-        });
+        title_text=findViewById(R.id.textView);
+        textView2=findViewById(R.id.textView2);
+        imageView=findViewById(R.id.imageView6);
+        imageView2=findViewById(R.id.imageView5);
 
         Thread thread=new Thread(){
             public void run(){
+                Animation animation= AnimationUtils.loadAnimation(splash_screen.this,R.anim.anim);
+                Animation animation2= AnimationUtils.loadAnimation(splash_screen.this,R.anim.fade_in);
+                title_text.startAnimation(animation);
+                textView2.startAnimation(animation2);
+                imageView2.startAnimation(animation2);
+                imageView.startAnimation(animation2);
+
                 try{
                     sleep(3000);
+
 
                     }
                 catch(Exception e){
