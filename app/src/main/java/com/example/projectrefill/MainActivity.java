7782d90b220c578411be200e69cbd2741e385559
Venugopal <venugpl2000@gmail.com>
2022-05-29 15:44:28 +0530

    Updated

--- app/src/main/java/com/example/projectrefill/MainActivity.java
+++ app/src/main/java/com/example/projectrefill/MainActivity.java
@@ -18,14 +18,53 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-
-
+    DatabaseReference databaseReference=FirebaseDatabase.getInstance().getReferenceFromUrl("https://project-refill-default-rtdb.asia-southeast1.firebasedatabase.app/");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button login=findViewById(R.id.btn_login);
+        EditText txtUser=findViewById(R.id.txtUser);
+        EditText txtPassword=findViewById(R.id.txtPassword);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String eUser=txtUser.getText().toString();
+                final String ePassword=txtPassword.getText().toString();
+
+                if(eUser.isEmpty()||ePassword.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Enter the credentials", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    databaseReference.child("Retailer").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            //check data exist in firebase
+                            if(snapshot.hasChild(eUser)){
+                                //match data
+                                final String getPassword=snapshot.child("eUSer").child("password").getValue(String.class);
+                                if(getPassword.equals("password")){
+                                    Toast.makeText(MainActivity.this, "Logged In", Toast.LENGTH_SHORT).show();
+                                }else{
+                                    Toast.makeText(MainActivity.this, "Wrong Password", Toast.LENGTH_SHORT).show();
+                                }
+                            }else{
+                                Toast.makeText(MainActivity.this, "Wrong Username", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+
+            }
+        });
 
     }
 
