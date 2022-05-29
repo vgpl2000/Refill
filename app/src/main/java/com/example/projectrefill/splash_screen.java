edda3ca676c4b374a03eac2f4d29db571cee0f88
Akash <akashcbhat2001@gmail.com>
2022-05-29 12:28:15 +0530

    video view added

--- app/src/main/java/com/example/projectrefill/splash_screen.java
+++ app/src/main/java/com/example/projectrefill/splash_screen.java
@@ -3,28 +3,31 @@ package com.example.projectrefill;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 public class splash_screen extends AppCompatActivity {
     TextView title_text;
-
+    VideoView videoView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        title_text=findViewById(R.id.title_text);
-
+        videoView.findViewById(R.id.videoView);
+        String videopath="android.resource://com.example.projectrefill/"+R.raw.splash;
+Uri uri=Uri.parse(videopath);
+videoView.setVideoURI(uri);
+        videoView.start();
         Thread thread=new Thread(){
             public void run(){
-                Animation animation= AnimationUtils.loadAnimation(splash_screen.this,R.anim.anim);
-                title_text.startAnimation(animation);
                 try{
                     sleep(3000);
-
-
                     }
                 catch(Exception e){
                     e.printStackTrace();
