9670e3d4a6b716080ecda8bb1ef8257cbfa0dc8a
Venugopal <venugpl2000@gmail.com>
2022-05-29 12:44:29 +0530

    video view (v 1.1)

--- app/src/main/java/com/example/projectrefill/splash_screen.java
+++ app/src/main/java/com/example/projectrefill/splash_screen.java
@@ -3,6 +3,7 @@ package com.example.projectrefill;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -13,21 +14,30 @@ import android.widget.TextView;
 import android.widget.VideoView;
 
 public class splash_screen extends AppCompatActivity {
-    TextView title_text;
+
     VideoView videoView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
         videoView.findViewById(R.id.videoView);
+
         String videopath="android.resource://com.example.projectrefill/"+R.raw.splash;
-Uri uri=Uri.parse(videopath);
-videoView.setVideoURI(uri);
-        videoView.start();
+        Uri uri=Uri.parse(videopath);
+        videoView.setVideoURI(uri);
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.start();
+            }
+        });
+
         Thread thread=new Thread(){
             public void run(){
                 try{
                     sleep(3000);
+
                     }
                 catch(Exception e){
                     e.printStackTrace();
