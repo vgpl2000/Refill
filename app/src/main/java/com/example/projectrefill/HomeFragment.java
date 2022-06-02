43e1e6be25e8f038f8259e99fffcced009e45f60
Akash <akashcbhat2001@gmail.com>
2022-06-02 15:48:17 +0530

    check orders button added and working halfv4

--- app/src/main/java/com/example/projectrefill/HomeFragment.java
+++ app/src/main/java/com/example/projectrefill/HomeFragment.java
@@ -58,6 +58,8 @@ public class HomeFragment extends Fragment {
         View v= inflater.inflate(R.layout.fragment_home, container, false);
 //recycler added
         //just for github purpose
+        //just for github purpose2
+
         recyclerView1=(RecyclerView) v.findViewById(R.id.recyclerView);
         recyclerView1.setLayoutManager(new LinearLayoutManager(getContext()));
 
