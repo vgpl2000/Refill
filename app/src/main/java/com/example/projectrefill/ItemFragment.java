102a8d8425f1bfe29ef999b63acf4aa097daaaa7
Venugopal <venugpl2000@gmail.com>
2022-06-07 17:41:19 +0530

    Searchview for items

--- app/src/main/java/com/example/projectrefill/ItemFragment.java
+++ app/src/main/java/com/example/projectrefill/ItemFragment.java
@@ -1,5 +1,6 @@
 package com.example.projectrefill;
 
+import android.app.SearchManager;
 import android.content.Context;
 import android.os.Bundle;
 
@@ -16,10 +17,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Locale;
 
 
 public class ItemFragment extends Fragment {
@@ -27,6 +31,7 @@ public class ItemFragment extends Fragment {
     Button btn_add_items;
     RecyclerView recyclerViewforitemdisplay;
     adapter_clientside_itemdisplaying adapter;
+    SearchView searchView;
 
 
 
@@ -47,6 +52,8 @@ public class ItemFragment extends Fragment {
         recyclerViewforitemdisplay.setAdapter(adapter);
 
 
+        searchView=v.findViewById(R.id.searchView_items);
+
         btn_add_items=v.findViewById(R.id.btn_add_items);
 
         btn_add_items.setOnClickListener(new View.OnClickListener() {
@@ -62,8 +69,44 @@ public class ItemFragment extends Fragment {
             }
         });
 
+
+        SearchManager searchManager= (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getActivity().getComponentName()));
+        searchView.setIconified(true);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+
+                mySearch(s);
+                searchView.clearFocus();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                mySearch(s);
+                return true;
+            }
+
+        });
+
+
         return  v;
     }
+
+    private void mySearch(String s) {
+
+        FirebaseRecyclerOptions<client_model_todisplayitemsavailable> options =
+                new FirebaseRecyclerOptions.Builder<client_model_todisplayitemsavailable>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("Client").child("c_items").orderByChild("name").startAt(s).endAt(s+"\uf8ff"), client_model_todisplayitemsavailable.class)
+                        .build();
+
+        adapter=new adapter_clientside_itemdisplaying(options);
+        adapter.startListening();
+        recyclerViewforitemdisplay.setAdapter(adapter);
+
+    }
+
     @Override
     public void onStart() {
         super.onStart();
