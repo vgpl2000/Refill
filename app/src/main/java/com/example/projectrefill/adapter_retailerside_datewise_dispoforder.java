2f7ada59803237674019b25b079340dd7f14bb1f
Venugopal <venugpl2000@gmail.com>
2022-06-26 20:32:42 +0530

    nothing much

--- app/src/main/java/com/example/projectrefill/adapter_retailerside_datewise_dispoforder.java
+++ app/src/main/java/com/example/projectrefill/adapter_retailerside_datewise_dispoforder.java
@@ -21,17 +21,22 @@ public class adapter_retailerside_datewise_dispoforder extends FirebaseRecyclerA
     protected void onBindViewHolder(@NonNull myviewholder holder, int position, @NonNull retailer_model_datewise_detailsdisp model) {
         holder.name.setText(model.getName());
         System.out.println(model.getName()+" name value");
+
         holder.price.setText(model.getPrice());
         System.out.println(model.getPrice()+" name value");
+
         holder.quan.setText(model.getQuan());
         System.out.println(model.getQuan()+" name value");
+
         holder.weight.setText(model.getWeight());
         System.out.println(model.getWeight()+" name value");
 
         String pri=holder.price.getText().toString();
         String qu=holder.quan.getText().toString();
+
         Integer pr1=Integer.parseInt(pri);
         Integer qr1=Integer.parseInt(qu);
+        
         Integer tot=pr1*qr1;
         String totalf=Integer.toString(tot);
         holder.totprice.setText(totalf);
