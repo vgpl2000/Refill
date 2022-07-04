2ece7c05ddd55ec093ec743acb44c6b7e4bd7888
Akash <akashcbhat2001@gmail.com>
2022-07-04 22:17:07 +0530

    notificaiton on progress part4.7

--- app/src/main/java/com/example/projectrefill/firebasetestmessaging.java
+++ app/src/main/java/com/example/projectrefill/firebasetestmessaging.java
@@ -31,121 +31,116 @@ public class firebasetestmessaging extends FirebaseMessagingService {
 
         String notificationtype=message.getData().get("notificationtype");
 
-        if(notificationtype.equals("orderplaced")){
-            Uri notification= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-            Ringtone r=RingtoneManager.getRingtone(getApplicationContext(),notification);
-            r.play();
-            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.P){
-                r.setLooping(false);
-            }
-            Vibrator v=(Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-            long[] pattern={100,300,300,300};
-            v.vibrate(pattern,-1);
+        try {
 
 
+            if (notificationtype.equals("orderplaced")) {
+                Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+                r.play();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                    r.setLooping(false);
+                }
+                Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                long[] pattern = {100, 300, 300, 300};
+                v.vibrate(pattern, -1);
 
-            NotificationCompat.Builder builder=new NotificationCompat.Builder(this,"CHANNEL_ID");
 
-            Bitmap largeicon= BitmapFactory.decodeResource(getResources(),R.drawable.logo);
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "CHANNEL_ID");
+
+                Bitmap largeicon = BitmapFactory.decodeResource(getResources(), R.drawable.logo);
 
 
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    builder.setSmallIcon(R.drawable.logo);
+                    builder.setLargeIcon(largeicon);
+                } else {
+                    builder.setSmallIcon(R.drawable.logo);
+                    builder.setLargeIcon(largeicon);
+                }
 
 
-            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP){
-                builder.setSmallIcon(R.drawable.logo);
+                Intent resultintent = new Intent(this, client_activity.class);
+
+
+                PendingIntent pendingIntent = PendingIntent.getActivity(this, 1, resultintent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                builder.setContentTitle(message.getNotification().getTitle());
+                builder.setContentText(message.getNotification().getBody());
+                builder.setContentIntent(pendingIntent);
+                builder.setStyle(new NotificationCompat.BigTextStyle().bigText(message.getNotification().getBody()));
+                builder.setAutoCancel(true);
+                builder.setPriority(Notification.PRIORITY_MAX);
                 builder.setLargeIcon(largeicon);
-            }else {
-                builder.setSmallIcon(R.drawable.logo);
+
+
+                mnotification = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    String Channel_id = "urchannelid";
+                    NotificationChannel channel = new NotificationChannel(Channel_id, "title", NotificationManager.IMPORTANCE_HIGH);
+                    mnotification.createNotificationChannel(channel);
+                    builder.setChannelId(Channel_id);
+
+                }
+
+                mnotification.notify(100, builder.build());
+
+            } else if (notificationtype.equals("accepted")) {
+                Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+                r.play();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                    r.setLooping(false);
+                }
+                Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                long[] pattern = {100, 300, 300, 300};
+                v.vibrate(pattern, -1);
+
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "CHANNEL_ID");
+
+                Bitmap largeicon = BitmapFactory.decodeResource(getResources(), R.drawable.logo);
+
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    builder.setSmallIcon(R.drawable.logo);
+                    builder.setLargeIcon(largeicon);
+                } else {
+                    builder.setSmallIcon(R.drawable.logo);
+                    builder.setLargeIcon(largeicon);
+                }
+
+
+                Intent resultintent = new Intent(this, retailer_activity.class);
+
+
+                PendingIntent pendingIntent = PendingIntent.getActivity(this, 1, resultintent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                builder.setContentTitle(message.getNotification().getTitle());
+                builder.setContentText(message.getNotification().getBody());
+                builder.setContentIntent(pendingIntent);
+                builder.setStyle(new NotificationCompat.BigTextStyle().bigText(message.getNotification().getBody()));
+                builder.setAutoCancel(true);
+                builder.setPriority(Notification.PRIORITY_MAX);
                 builder.setLargeIcon(largeicon);
+
+
+                mnotification = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    String Channel_id = "urchannelid";
+                    NotificationChannel channel = new NotificationChannel(Channel_id, "title", NotificationManager.IMPORTANCE_HIGH);
+                    mnotification.createNotificationChannel(channel);
+                    builder.setChannelId(Channel_id);
+
+                }
+
+                mnotification.notify(100, builder.build());
             }
-
-
-
-            Intent resultintent=new Intent(this,client_activity.class);
-
-
-
-            PendingIntent pendingIntent=PendingIntent.getActivity(this,1,resultintent,PendingIntent.FLAG_UPDATE_CURRENT);
-
-            builder.setContentTitle(message.getNotification().getTitle());
-            builder.setContentText(message.getNotification().getBody());
-            builder.setContentIntent(pendingIntent);
-            builder.setStyle(new NotificationCompat.BigTextStyle().bigText(message.getNotification().getBody()));
-            builder.setAutoCancel(true);
-            builder.setPriority(Notification.PRIORITY_MAX);
-            builder.setLargeIcon(largeicon);
-
-
-            mnotification=(NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
-
-            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
-                String Channel_id="urchannelid";
-                NotificationChannel channel=new NotificationChannel(Channel_id,"title",NotificationManager.IMPORTANCE_HIGH);
-                mnotification.createNotificationChannel(channel);
-                builder.setChannelId(Channel_id);
-
-            }
-
-            mnotification.notify(100,builder.build());
-
-        }else if(notificationtype.equals("accepted")){
-            Uri notification= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-            Ringtone r=RingtoneManager.getRingtone(getApplicationContext(),notification);
-            r.play();
-            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.P){
-                r.setLooping(false);
-            }
-            Vibrator v=(Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-            long[] pattern={100,300,300,300};
-            v.vibrate(pattern,-1);
-
-
-
-            NotificationCompat.Builder builder=new NotificationCompat.Builder(this,"CHANNEL_ID");
-
-            Bitmap largeicon= BitmapFactory.decodeResource(getResources(),R.drawable.logo);
-
-
-
-
-            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP){
-                builder.setSmallIcon(R.drawable.logo);
-                builder.setLargeIcon(largeicon);
-            }else {
-                builder.setSmallIcon(R.drawable.logo);
-                builder.setLargeIcon(largeicon);
-            }
-
-
-
-            Intent resultintent=new Intent(this,retailer_activity.class);
-
-
-
-            PendingIntent pendingIntent=PendingIntent.getActivity(this,1,resultintent,PendingIntent.FLAG_UPDATE_CURRENT);
-
-            builder.setContentTitle(message.getNotification().getTitle());
-            builder.setContentText(message.getNotification().getBody());
-            builder.setContentIntent(pendingIntent);
-            builder.setStyle(new NotificationCompat.BigTextStyle().bigText(message.getNotification().getBody()));
-            builder.setAutoCancel(true);
-            builder.setPriority(Notification.PRIORITY_MAX);
-            builder.setLargeIcon(largeicon);
-
-
-            mnotification=(NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
-
-            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
-                String Channel_id="urchannelid";
-                NotificationChannel channel=new NotificationChannel(Channel_id,"title",NotificationManager.IMPORTANCE_HIGH);
-                mnotification.createNotificationChannel(channel);
-                builder.setChannelId(Channel_id);
-
-            }
-
-            mnotification.notify(100,builder.build());
+        }catch (Exception e){
+            e.printStackTrace();
         }
-
 
 
     }
