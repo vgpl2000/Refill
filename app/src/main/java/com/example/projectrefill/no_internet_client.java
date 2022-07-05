0d371275eaf0ab00b0807ad7481f30807c2e7a8c
Venugopal <venugpl2000@gmail.com>
2022-07-05 20:23:38 +0530

    fix on backpressed on no internet activity

--- app/src/main/java/com/example/projectrefill/no_internet_client.java
+++ app/src/main/java/com/example/projectrefill/no_internet_client.java
@@ -27,4 +27,19 @@ public class no_internet_client extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public void onBackPressed () {
+        int count = getSupportFragmentManager().getBackStackEntryCount();
+
+        if (count == 0) {
+            super.onBackPressed();
+            Intent startMain = new Intent(Intent.ACTION_MAIN);
+            startMain.addCategory(Intent.CATEGORY_HOME);
+            startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(startMain);
+        } else {
+            getSupportFragmentManager().popBackStack();
+        }
+    }
 }
