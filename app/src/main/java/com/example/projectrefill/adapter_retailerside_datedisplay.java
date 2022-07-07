48be53a6c92ca0fe059a5ce06695cc14dc46dd2d
Venugopal <venugpl2000@gmail.com>
2022-07-07 18:48:02 +0530

    fixed prblm in retailer_activity.java

--- app/src/main/java/com/example/projectrefill/adapter_retailerside_datedisplay.java
+++ app/src/main/java/com/example/projectrefill/adapter_retailerside_datedisplay.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
